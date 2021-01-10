f876fd782883401d1aef33cd499a25284686d091
Mandy Hom <amandakhom@gmail.com>
2021-01-09 22:09:33 -0800

    add degree format, toggles, icons

--- assets/components/main.tsx
+++ assets/components/main.tsx
@@ -1,56 +1,100 @@
 import React, { useState } from 'react'
 import DataResults from '../../types/interface'
 
-async function getDetails(textInput: string) {
-  try {
-    const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=314c762603e28d1740cdcb0db478d25c&units=standard`,
-      { mode: 'cors' },
-    )
-    const data = await response.json()
-    console.log(data)
-    return getWeather(data)
-  } catch (err) {
-    console.log(err)
-  }
-  return
-}
-
-function getWeather(data: DataResults) {
-  const cityAndCountry = `${data['name']}, ${data['sys']['country']}`
-  const description = `${data['weather'][0]['description']}`
-  const temperature = `Feels like: ${data['main']['feels_like']}, Humidity: ${data['main']['humidity']}, Temp: ${data['main']['temp']}`
-  const wind = parseInt(`${data['wind']['speed']}`)
-  // console.log(cityAndCountry, description, temperature, wind)
-  return { cityAndCountry, description, temperature, wind }
-}
-
-interface CityInformation {
-  cityAndCountry: string
-  description: string
-  temperature: string
-  wind: number
-}
-
 function Main() {
-  const [cityInformation, setCityInformation] = useState<CityInformation>()
+  const [cityInformation, setCityInformation] = useState<CityInformationInterface>()
   const [city, setCity] = useState('')
   const [error, setError] = useState('')
+  const [tempInformation, setTempInformation] = useState<CityTempInterface>()
+  const [click, setClick] = useState(false)
+
   const onSubmit = async (event: any) => {
     event.preventDefault()
     if (city) {
       let weather = await getDetails(city)
       setCityInformation(weather)
+      setTempInformation(weather)
     } else {
-      setError('ERROR: CITY IS REQUIRED')
+      setError('A city is required. Please try again.')
     }
+  }
+
+  const handleClick = async (event: any) => {
+    event.preventDefault()
+    if (tempInformation) {
+      setClick(!click)
+    }
+  }
+
+  async function getDetails(textInput: string) {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${textInput}&appid=314c762603e28d1740cdcb0db478d25c&units=standard`,
+        { mode: 'cors' },
+      )
+      const data = await response.json()
+      //   console.log(data)
+      return getWeather(data)
+    } catch (err) {
+      console.log(err)
+      setError('Cannot find city. Please try again.')
+    }
+    return
+  }
+
+  function getWeather(data: DataResults) {
+    const cityAndCountry = `${data['name']}, ${data['sys']['country']}`
+    const description = `${data['weather'][0]['description']}`
+    const feelsLike: number = parseInt(`${data['main']['feels_like']} `)
+    const humidity: number = parseInt(`${data['main']['humidity']}`)
+    const temperature: number = parseInt(`${data['main']['temp']}`)
+    const wind: number = parseInt(`${data['wind']['speed']}`)
+    const icon = `http://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png`
+    console.log(icon)
+
+    // Convert to degrees
+    const feelsLikeCelsius = Math.round(feelsLike - 273.15)
+    const temperatureCelsius = Math.round(temperature - 273.15)
+    const feelsLikeFahrenheit = Math.round((feelsLikeCelsius * 9) / 5) + 32
+    const temperatureFahrenheit = Math.round((temperatureCelsius * 9) / 5) + 32
+
+    return {
+      cityAndCountry,
+      description,
+      feelsLikeCelsius,
+      temperatureCelsius,
+      feelsLikeFahrenheit,
+      temperatureFahrenheit,
+      humidity,
+      wind,
+      icon,
+    }
+  }
+
+  interface CityInformationInterface {
+    cityAndCountry: string
+    description: string
+    icon: string
+  }
+
+  interface CityTempInterface {
+    feelsLikeCelsius: number
+    temperatureCelsius: number
+    feelsLikeFahrenheit: number
+    temperatureFahrenheit: number
+    humidity: number
+    wind: number
   }
 
   return (
     <div className="container">
       <div className="header">
         <div className="icon-container">
-          <canvas id="icon" width="100" height="100"></canvas>
+          {cityInformation && (
+            <div className="icon">
+              <img src={cityInformation.icon} alt="weather icon" width={100} height={100} />
+            </div>
+          )}
         </div>
       </div>
 
@@ -60,11 +104,18 @@ function Main() {
           <div className="location"></div>
         </div>
         {cityInformation && (
-          <div className="detail-section">
+          <div className="cityDetails">
             {cityInformation.cityAndCountry}
             {cityInformation.description}
-            {cityInformation.temperature}
-            {cityInformation.wind}
+          </div>
+        )}
+        {tempInformation && (
+          <div className="cityTempDetails">
+            Temperature: {click ? tempInformation.temperatureFahrenheit : tempInformation.temperatureCelsius}
+            {click ? 'F' : 'C'}
+            Feels Like: {click ? tempInformation.feelsLikeFahrenheit : tempInformation.feelsLikeCelsius}
+            {click ? 'F' : 'C'}
+            Humidity: {tempInformation.humidity}% Wind: {tempInformation.wind} mph
           </div>
         )}
         {/* Display city when typed
@@ -82,6 +133,9 @@ function Main() {
 
         <button type="button" onClick={onSubmit}>
           Submit!
+        </button>
+        <button type="button" onClick={handleClick}>
+          {click ? 'F' : 'C'}
         </button>
       </div>
     </div>
